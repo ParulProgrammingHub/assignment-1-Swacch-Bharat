927f300a794eed35bc03fda3471aaabca663ceaa
Mritesh Adak <150303105001@paruluniversity.ac.in>
2017-02-10 21:10:15 +0530

    assignment 1 : question 1

---
+++ a1q1.py
@@ -0,0 +1,5 @@
+length = input('Enter length of rectangle : ')
+breadth = input('Enter breadth of rectangle : ')
+perimeter = 2*(length+breadth)
+area = length*breadth
+print 'Perimeter of rectangle is ',perimeter,'\nArea of rectangle is ',area
